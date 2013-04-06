e8b8df1522ec92799393a8954fc4754c5fb62595
Jessica Bethune <jessica.bethune@gmail.com>
2013-04-06 14:46:43 -0400

    can search for and then play songs using node-rdio

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
-/*
- * GET home page.
- */
+var Rdio = require('node-rdio');
 
 exports.index = function(req, res){
-  res.render('music', { title: 'Express' });
+	var rdio = new Rdio([process.env.CONSUMER_KEY, process.env.CONSUMER_SECRET]);
+	rdio.call('search', {'query': 'all night longer', 'types': 'Track'}, function(err, songs) {
+		res.render('music', { title: 'Musica', id: songs.result.results[0].key });
+	});
 };
